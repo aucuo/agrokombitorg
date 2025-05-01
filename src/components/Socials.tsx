33b9496d647acb95ba217f86bf8885435522645f
aucuo <jahor.sykaviec@gmail.com>
2025-05-01 11:15:30 +0300

    socials edit & title span fix

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -3,28 +3,23 @@ import './Socials.scss';
 
 import {ReactSVG} from "react-svg";
 import UButton from "./UButton.tsx";
-import XTwitterIcon from "@/assets/icons/x-twitter.svg";
-import WhatsappIcon from "@/assets/icons/whatsapp.svg";
+import TiktokIcon from "@/assets/icons/tiktok.svg";
+import InstagramIcon from "@/assets/icons/instagram.svg";
 import VKIcon from "@/assets/icons/vk.svg";
-import TelegramIcon from "@/assets/icons/telegram.svg";
 
 const Socials: React.FC = () => {
     const socials = [
         {
-            icon: XTwitterIcon,
-            href: '#'
+            icon: TiktokIcon,
+            href: 'https://www.tiktok.com/@zlou300'
         },
         {
-            icon: WhatsappIcon,
+            icon: InstagramIcon,
             href: '#'
         },
         {
             icon: VKIcon,
-            href: '#'
-        },
-        {
-            icon: TelegramIcon,
-            href: '#'
+            href: 'https://vk.com/id885072417'
         },
     ];
 
@@ -32,9 +27,11 @@ const Socials: React.FC = () => {
         <div className="socials">
             {
                 socials.map((item, index) => (
-                    <UButton variant="transparent-white" size="circle" key={index}>
-                        <ReactSVG className="reactsvg" src={item.icon}/>
-                    </UButton>
+                    <a href={item.href} target="_blank" rel="noopener noreferrer" key={index}>
+                        <UButton variant="transparent-white" size="circle" key={index}>
+                            <ReactSVG className="reactsvg" src={item.icon}/>
+                        </UButton>
+                    </a>
                 ))
             }
         </div>
